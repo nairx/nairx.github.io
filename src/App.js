01567f2bd28e6158140c4439c55a26e0dd4c9b27
praveen <nairxprv@gmail.com>
2024-03-14 21:53:42 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello! After CI is done</h1>
+      <div className="leftBox">
+        <h1>Praveen Nair</h1>
+        <p>I am a MERN stack developer.</p>
+      </div>
+      <div className="rightBox"><h1>Projects</h1>
+      <li><a href="https://nairsite.github.io/ecomm-react">E-commerce</a></li>
+      <li><a href="https://nairsite.github.io/social-react">Social Media</a></li>
+      </div>
     </div>
   );
 }
