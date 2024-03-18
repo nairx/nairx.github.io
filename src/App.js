9630424c4dbac1899f243d92810ff3ca9b0b4341
praveen <nairxprv@gmail.com>
2024-03-18 18:36:14 +0530

    Added social links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import './App.css';
-
+import "./App.css";
+import { FaGithubSquare, FaLinkedin, FaYoutubeSquare } from "react-icons/fa";
 function App() {
   return (
     <div className="App">
       <div className="leftBox">
         <h1>Praveen Nair</h1>
-        <p>I am a MERN stack developer.</p>
+        <p>MERN Stack Trainer & Developer</p>
+        <div className="App-icons">
+          <a href="https://github.com/nairsite">
+            <FaGithubSquare />
+          </a><a href="https://in.linkedin.com/in/nairprv">
+            <FaLinkedin />
+          </a><a href="https://www.youtube.com/@praveen-nair">
+            <FaYoutubeSquare />
+          </a>
+        </div>
       </div>
-      <div className="rightBox"><h1>Projects</h1>
-      <li><a href="https://nairsite.github.io/ecomm-react">E-commerce</a></li>
-      <li><a href="https://nairsite.github.io/social-react">Social Media</a></li>
+      <div className="rightBox">
+        <h1>Projects</h1>
+        <li>
+          <a href="https://nairsite.github.io/ecomm-react">E-commerce</a>
+        </li>
+        <li>
+          <a href="https://nairsite.github.io/social-react">Social Media</a>
+        </li>
       </div>
     </div>
   );
