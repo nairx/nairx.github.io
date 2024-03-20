47b238b6b5970efc56484a9dee0d6b21ac4aa3de
praveen <nairxprv@gmail.com>
2024-03-20 09:10:45 +0530

    Added description

--- src/App.js
+++ src/App.js
@@ -5,9 +5,33 @@ function App() {
     <>
       <div className="App">
         <div className="leftBox">
-          <h1>Praveen Nair</h1>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div>
+              <h1>Praveen Nair</h1>
+            </div>
+            <div>
+              <h1>
+                <a href="https://github.com/nairsite">
+                  <FaGithubSquare />
+                </a>
+                <a href="https://in.linkedin.com/in/nairprv">
+                  <FaLinkedin />
+                </a>
+                <a href="https://www.youtube.com/@praveen-nair">
+                  <FaYoutubeSquare />
+                </a>
+              </h1>
+            </div>
+          </div>
           <div className="App-title">MERN Stack Trainer & Developer</div>
-          <br></br>
+          <p className="App-companies">
+            Over 20 years of experience as Trainer, Application Developer and
+            Project Manager in India and USA. I have primarily worked for Wipro
+            and JP Morgan Chase and currently coach freshers on web development
+            technologies such as Reactjs, Nodejs, Expressjs, MongoDB, MySQL,
+            Bootstrap, CSS, JavaScript and HTML.
+          </p>
+          {/* <br></br>
           <div className="App-icons">
             <a href="https://github.com/nairsite">
               <FaGithubSquare />
@@ -18,8 +42,8 @@ function App() {
             <a href="https://www.youtube.com/@praveen-nair">
               <FaYoutubeSquare />
             </a>
-          </div>
-          <h1>Clients</h1>
+          </div> */}
+          <h1>Top Clients</h1>
           <div className="App-companies">
             Wipro | Besant Technologies | Oxford College | Elewayte | Zognu
             Technologies | NetRichh IT Solutions | Cloud Soft Solutions | Vidya
