f5d871d49104d4b6c96e184712014bfc38ec0498
nairx <nairxprv@gmail.com>
2025-01-09 13:33:47 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ export default function App() {
               <li>
                 <a href="#home">Home</a>
               </li>
-              {/* <li>
-                <a href="#about">About</a>
-              </li> */}
+              <li>
+                <a href="#course">Courses</a>
+              </li>
 
               <li>
                 <a href="#clients">Clients</a>
@@ -24,7 +24,7 @@ export default function App() {
                 <a href="#projects">Projects</a>
               </li>
 
-              <li>
+              <li style={{backgroundColor:'white',borderRadius:'10px'}}>
                 <a href="#contact">Contact</a>
               </li>
             </ul>
@@ -45,38 +45,91 @@ export default function App() {
               <button>Login</button>
             </div>
             <div className="box">
-              <img src="https://picsum.photos/id/1/400/400" alt="Title" />
+              <img src="wallpaper.png" alt="progress" width="100%" />
             </div>
           </div>
         </div>
 
-        {/* <div
-          id="about"
+        <div
+          id="course"
           className="row"
           style={{ backgroundColor: "lightcoral" }}
         >
-          <h1>About</h1>
-          <h3>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sint
-            optio labore a obcaecati. Ullam distinctio corrupti autem. Veritatis
-            ipsa veniam deserunt asperiores nesciunt at provident itaque
-            cupiditate vel dolor.
-          </h3>
-          <h2>Skills</h2>
-          <h3>Java, Python, Reactjs</h3>
-          <p>Display the logo of all the technologies you know.</p>
-        </div> */}
+          <h2>MERN Stack Crash Course</h2>
+          <div
+            class="c"
+            style={{
+              display: "flex",
+              justifyContent: "center",flexWrap:"wrap",
+              margin: "10px",
+            }}
+          >
+            <div>
+              <li className="course">
+                <b>Module 1: Introduction to MERN Stack</b><hr></hr>
+                Setting up the environment.<br></br>
+                Node.js and npm installation.<br></br>
+                Code editors (e.g., VS Code).<br></br>
+                Introduction to GitHub.<br></br>
+                Basics of HTML, CSS and JavaScript
+              </li>
+              <li className="course">
+                <b>Module 4: Frontend Development</b><hr></hr>
+                Components, Props, and State.<br></br>
+                React hooks (useState, useEffect).<br></br>
+                State management with Context API.<br></br>
+                React Router for navigation.<br></br>
+                Fetching data from APIs using Axios.<br></br>
+              </li>
+             
+            </div>
+            <div>
+            <li className="course">
+                <b>Module 2: MongoDB Basics</b><hr></hr>
+                Introduction to NoSQL databases.<br></br>
+                CRUD operations using MongoDB.<br></br>
+                Connecting MongoDB with Node.js.<br></br>
+                Setting up MongoDB Atlas.<br></br>
+              </li>
+             
+              <li className="course">
+                <b>Module 5: Deployment and Best Practices</b><hr></hr>
+                Deploying the Frontend App.<br></br>
+                Deploying the Backend App.<br></br>
+                Environment variables and security.<br></br>
+              </li>
+            </div>
+            <div>
+            <li className="course">
+                <b>Module 3: Backend Development</b><hr></hr>
+                Overview of top backend technologies.<br></br>
+                Introduction to Nodejs.<br></br>
+                Introduction to Express.js and RESTful APIs.<br></br>
+                Building APIs: GET, POST, PUT, DELETE.<br></br>
+                Middleware in Express.js<br></br>
+              </li>
+              
+              <li className="course">
+                <b>Module 6: E-commerce Store Project</b><hr></hr>
+                Product listing<br></br>
+                Cart functionality<br></br>
+                Backend integration.<br></br>
+              </li>
+            </div>
+          </div>
+        </div>
 
         <div
           id="clients"
           className="row"
           style={{ backgroundColor: "lightblue" }}
         >
-          <h1>Clients</h1>
-          <div class="c"
+          <h2>Trusted By Top Institutes Across India</h2>
+          <div
+            class="c"
             style={{
               display: "flex",
-              justifyContent: "center",
+              justifyContent: "center",flexWrap:"wrap",
               margin: "10px",
             }}
           >
@@ -85,10 +138,16 @@ export default function App() {
               <li>Besant Technologies, Bangalore</li>
               <li>Oxford College, Bangalore</li>
               <li>Elewayte, Bangalore</li>
+              <li>Student Tribe, Hyderabad</li>
+              <li>GRIET College, Hyderabad</li>
+            </div>
+            <div>
               <li>Zognu Technologies, Hyderabad</li>
               <li>NetRichh IT Solutions, Hyderabad</li>
               <li>Cloud Soft Solutions, Hyderabad</li>
               <li>Vidya Academy, Kerala</li>
+              <li>Lyros Technologies, Hyderabad</li>
+              <li>GoCinQ, Hyderabad </li>
             </div>
             <div>
               <li>Aptech Computer Education, Kerala</li>
@@ -96,35 +155,28 @@ export default function App() {
               <li>Imarticus Learning, Hyderabad</li>
               <li>Blackbucks, Hyderabad</li>
               <li> Malla Reddy University, Hyderabad</li>
-              <li>GoCinQ, Hyderabad </li>
-              <li>Lyros Technologies, Hyderabad</li>
-              <li>Student Tribe, Hyderabad</li>
+              <li>Mahindra University, Hyderabad</li>
             </div>
             <div>
               <li>CareerX Club, Hyderabad</li>
               <li>Achievers IT, Hyderabad</li>
               <li>Numeric Technologies, USA</li>
               <li>Cantilever Labs, Hyderabad</li>
-              <li>Mahindra University, Hyderabad</li>
-              <li>GRIET College, Hyderabad</li>
             </div>
           </div>
         </div>
 
         <div id="projects" className="row">
-          <h1>Projects</h1>
+          <h2>Projects</h2>
           <div style={{ display: "flex" }}>
-            <div className="box">
-              <h3>E-commerce</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-                quia nobis eligendi reiciendis. Esse a possimus eum est
-                veritatis aliquid ut eveniet. Et illum odio ut voluptates
-                molestiae? Perspiciatis, ab.
-              </p>
-              <a href="https://nairx.github.io/ecomm-react/" target="_blank">
-                View Project
-              </a>
+            <div className="box" style={{ border: "1px solid silver" }}>
+              <h3>
+                E-commerce |{" "}
+                <a href="https://nairx.github.io/ecomm-react/" target="_blank">
+                  View Demo
+                </a>
+              </h3>
+              <img src="ecomm.jpg" width="400px" />
             </div>
             <div className="box">
               <h3>Project 2</h3>
@@ -146,15 +198,14 @@ export default function App() {
             </div>
           </div>
         </div>
-        <div
+        {/* <div
           id="contact"
           className="row"
           style={{ backgroundColor: "lightgoldenrodyellow" }}
         >
-          <h1>Contact</h1>
+          <h2>Contact</h2>
           <div style={{ display: "flex" }}>
             <div className="box">
-              <h3>Contact Form</h3>
               <p>
                 <input type="text" placeholder="Enter Name" />
               </p>
@@ -171,23 +222,24 @@ export default function App() {
               <button>Send</button>
             </div>
             <div className="box">
-              <h3>Your/Company Name</h3>
               <p>
-                <b>Address: </b>Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Dolorem quia nobis eligendi reiciendis. Esse a
-                possimus eum est veritatis aliquid ut eveniet. Et illum odio ut
-                voluptates molestiae? Perspiciatis, ab.
+                <b>Address: </b>This is a sample address and can be replaced.
               </p>
               <p>
                 <b>Email Address: </b>sample@email.com
               </p>
-              <p>
-                <b>Phone Number: </b>+91 343 445 6789
-              </p>
+              
             </div>
           </div>
+        </div> */}
+
+        <div id="contact" className="footer">
+          <p>Accessibility</p>
+          <p>Email: praveen@email.com</p>
+          <p> Phone: 123-123-1234</p>
+          <p>Copyright © 2024 Praveen Teachings., All rights reserved.</p>
+          <p>Terms and Conditions | Privacy Policy</p>
         </div>
-        <div className="footer">@copyright 2024. All rights reserved.</div>
       </div>
     </>
   );
