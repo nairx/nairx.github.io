190139b91fd4e3fe43db97083b978e516ef7025b
nairx <nairxprv@gmail.com>
2025-01-21 22:43:08 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -37,8 +37,8 @@ export default function App() {
         <div className="row" id="home">
           <div className="flex">
             <div className="box">
-              <h1>MERN | MEAN | Python | Java</h1>
-              <h3>Full Stack Corporate Trainer</h3>
+              <h1>MERN Stack Corporate Trainer</h1>
+              <h3>MongoDB | Expressjs | Reactjs | Nodejs</h3>
               <p>
                 Over 20 years of experience as Trainer, Application Developer
                 and Project Manager in India and USA. Primarily worked for Wipro
